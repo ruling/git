7cc94ee7bcefe48fcf4d69f7231a206edb0f5994
Jiang Xin <worldhello.net@gmail.com>
2013-05-09 12:15:34 +0800

    git-clean: xxxxxx : test data in clean-test/ dir
    Signed-off-by: Jiang Xin <worldhello.net@gmail.com>

---
+++ clean-test/test.sh
@@ -0,0 +1,28 @@
+touch .gitignore
+touch doc/doc1.txt
+touch doc/req/doc2.txt
+touch src/include/src1.h
+touch src/src1.java
+touch src/src2.java
+touch test.sh
+
+mkdir -p doc/tmp1
+mkdir -p doc/tmp2
+mkdir -p src/tmp3
+mkdir -p src/tmp4
+
+touch 	 doc/001.txt
+touch 	 "doc/jiang+xin's \"file\".txt"
+touch 	 doc/901.o
+touch 	 doc/req/002.txt
+touch 	 doc/req/902.o
+touch 	 doc/tmp1/003.txt
+touch 	 doc/tmp1/903.o
+
+touch 	 src/004.txt
+touch 	 src/904.o
+touch 	 src/include/005.txt
+touch 	 src/include/905.o
+touch 	 src/tmp3/006.txt
+touch 	 src/tmp3/906.o
+
